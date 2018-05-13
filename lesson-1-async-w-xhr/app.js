9c655c1dde22de8ee85b1f53b28d7cd02ccb469c
danwise91 <danielwise19918@gmail.com>
2018-05-13 10:30:12 -0400

    not updating correctly

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -1,7 +1,7 @@
 (function () {
     const form = document.querySelector('#search-form');
     const searchField = document.querySelector('#search-keyword');
-    let searchedForText = 'hippos';
+    let searchedForText;
     const responseContainer = document.querySelector('#response-container');
  
 
@@ -11,9 +11,9 @@
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
        
-        $.ajax({
-   			 url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`
-		}).done(addImage);
+  //       $.ajax({
+  //  			 url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`
+		// }).done(addImage);
      //    const unsplashRequest = new XMLHttpRequest();
 	    // unsplashRequest.onload = addImage;
 	    // unsplashRequest.onerror = function(err){
@@ -25,12 +25,23 @@
 	    // unsplashRequest.setRequestHeader('Authorization', 'Client-ID de8dc930833f844d8e365e86a0c9712a04797d3026dfbafdf5c0ef8df2be4ef1');
 	    // unsplashRequest.send();
 
-		const articleRequest = new XMLHttpRequest();
-		articleRequest.onload = addArticles;
-		articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=2947b6fe5e6f47aa81d76e5003eda120`);
-		articleRequest.send();
+		// const articleRequest = new XMLHttpRequest();
+		// articleRequest.onload = addArticles;
+		// articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=2947b6fe5e6f47aa81d76e5003eda120`);
+		// articleRequest.send();
     });
+
+    fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`, {
+    headers: {
+        Authorization: 'Client-ID 79771e28ad522e16587d12e5b33947fe4a516fbded23b62071fb566b43807739'
+     }
+	}).then(response => response.json())
+		.then(addImage)
+		.catch(e => requestError(e, 'image'));
+
     function addImage(data){
+    	console.log(data);
+    	// debugger;
     	let htmlContent = '';
 		// let htmlContent = '';
 		// const data = JSON.parse(this.responseText);
@@ -46,8 +57,20 @@
 			}
 			responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
 		}
+	function requestError(e, part){
+		console.log(e);
+		responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning">Oh no! There was an error making a request for the ${part}.</p>`)
+	}
+
+	//use fetch request to get articles from NYT
+	fetch(`http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=2947b6fe5e6f47aa81d76e5003eda120`)
+	.then(response => response.json())
+	.then(addArticles)
+	.catch(e => requestErrorArticle(e, 'article'));
 
 	function addArticles (data) {
+		console.log(data);
+		// debugger;
 		let htmlContent = '';
 		// const data = JSON.parse(this.responseText);
 		// const firstArticle = articles;
@@ -63,6 +86,10 @@
 			}
 			responseContainer.insertAdjacentHTML('beforeend', htmlContent);
 		}
+	function requestErrorArticle(e, part){
+		console.log(e);
+		responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning">Oh no! There was an error making a request for the ${part}.</p>`)
+	}
 
 })();
 
